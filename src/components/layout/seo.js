a9b068f213459a6e59edddf27c09f89be91fecc4
Gary vision <will@gotradie.com.au>
2023-08-01 13:18:25 +1000

    Social meta changes

--- src/components/layout/seo.js
+++ src/components/layout/seo.js
@@ -1,19 +1,18 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-
 import icon from '/static/images/favicon/favicon.png'
-import socialBanner from '/static/images/meta-image.jpg'
+import socialBanner from '/static/images/Website-meta.png'
 
 const Metadata = () => (
   <div>
     <Helmet>
-      <title>We've got tradie chat sorted</title>
+      <title>The Messenger App Built For Work 🔨</title>
 
       <meta property='og:image' content={ socialBanner } />
       <meta property='og:locale' content='en_US' />
       <meta property='og:type' content='website' />
-      <meta property='og:title' content="We've got tradie chat sorted" />
-      <meta property='og:description' content="Simplify the way your team, subbies and worksites connect, communicate & collaborate in a purpose built messaging app" />
+      <meta property='og:title' content="The Messenger App Built For Work 🔨" />
+      <meta property='og:description' content="Simplify the way your team, worksites and clients communicate & more." />
       <meta property='og:url' content='https://www.gotradie.com.au' />
     </Helmet>
   </div>
