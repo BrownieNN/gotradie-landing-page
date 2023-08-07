d166ece0cf69da46b5a0c608900b0f4efca1f9c3
Gary vision <will@gotradie.com.au>
2023-08-07 18:23:09 +1000

    SEO image specific URL

--- src/pages/pricing.js
+++ src/pages/pricing.js
@@ -10,7 +10,7 @@ function Landscapers() {
   const seoProps = {
     title: "A fair Price for all businesses",
     description: "No nonense pricing, start for free no credit card needed",
-    imageUrl: "/static/images/Website-meta.png",
+    imageUrl: "https://www.gotradie.com.au/static/images/Website-meta.png", // Use the absolute URL
     imageWidth: 1200, // Specify the image width
     imageHeight: 630, // Specify the image height
     url: "https://www.gotradie.com.au/pricing",
