f992637ecd9dea0a3e48d21c0d7475a29f533dae
Gary vision <will@gotradie.com.au>
2023-10-27 13:26:21 +1100

    update questions

--- src/data/questionData.js
+++ src/data/questionData.js
@@ -1,29 +1,37 @@
 export const questionsData = [
-    {
-      question: "What is GoTradie?",
-      answer: "In simple terms GoTradie is your messaging app for your WORK. All your photos, videos, docos and more in one place no need to scroll through endless whatsapp groups, email chains, your phones gallery. Simplify all your work comms and media in one place",
-      open: true,
-    },
-    {
-      question: "Who can I message?",
-      answer: "You can invite WHOEVER you work with from your team on the ground, your subbies, clients and suppliers in one place."
-    },
-    {
-      question: "How much will it cost my business?",
-      answer: "It's totally FREE to start, you can invite as as many people as you like to your business once you go over your storage limit we'll send you an in app message to upgrade, none of your data will be lost EVER"
-    },
-    {
-        question: "Will it clog up my phone storage?",
-        answer: "Nope it's safely stored in the cloud meaning you can keep your works messages and media seperate to your private stuff. NOTHING is ever deleted from GoTradie"
-    },
-    {
-        question: "Is this another HiPages?",
-        answer: "Absolutely not, GoTradie was built to help you RUN your business enabling you better vision on what's happening on the ground when it comes to day to day communication with your team. Helping you do more with less"
-    },
-    {
-        question: "Can i see my messages on my computer?",
-        answer: "Ken oath you can! GoTradie also works on the browser saving you time going from site to site! And it's absolutely free with any plan"
-    },
+  {
+    question: "What is GoTradie?",
+    answer: "In simple terms GoTradie is your messaging app for your WORK. All your photos, videos, docos and more in one place no need to scroll through endless whatsapp groups, email chains, your phones gallery. Simplify all your work comms and media in one place",
+  }, 
+  {
+    question: "Why should I switch from Whatsapp?",
+    answer: "Efficiently manage your inbox by separating team, worksite, and client chats. Organize photos & videos with our 'clips' feature, and enhance team accountability with built-in to-do lists, eliminating endless message scrolling.",
+    open: true,
+  },
+  {
+    question: "How much will it cost my business?",
+    answer: "TRY FOR FREE, with upgrade options available checkout our pricing page for more info."
+  },
+  { 
+    question: "How long does it take to setup?",
+    answer: "Creating an account takes less than 60 seconds, through either our website or app stores. Once completed, just add your team and your up & running."
+  },
+  {
+    question: "Who can I message?",
+    answer: "You can invite WHOEVER you work with from your team on the ground, your subbies, clients and even connect with other businesses. Using our FREE directory feature."
+  },
+  {
+    question: "Will it work for my office staff or is it just for use on mobile?",
+    answer: "Ken oath! GoTradie also works on mobile, tablet and desktop saving you time going from site to site! It's available across all plans"
+  },
+  { 
+    question: "Will it clog up my phone storage?",
+    answer: "Nope, as the business owner you own all the data which is safely backed up in the cloud. So if someone leaves your company, lose their phone or don’t backup they don’t take all that info with them."
+  },
+  { 
+    question: "Does it integrate with other systems?",
+    answer: "Not yet, but we're working on it"
+  },
     // Add more questions and answers as needed
   ];
   
