8fbeac8f28297f0d7a3f295afcd809d58a5e29c4
Gary vision <will@gotradie.com.au>
2023-11-16 09:44:37 +1100

    adding package file

--- src/data/losengeData.js
+++
@@ -1,6 +0,0 @@
-// lozengeData.js
-export const losengeData = [
-    { label: 'Lozenge 1', anchorLink: '#section1' },
-    { label: 'Lozenge 2', anchorLink: '#section2' },
-    // ... other lozenges
-  ];
